4ca90c71b48a4894f10ae50e34e2db6e5741e2a0
Quentin DUPERON <qduperon@e1r2p11.42.fr>
2016-06-17 17:48:07 +0200

    push

--- Init_project.sh
+++ Init_project.sh
@@ -6,25 +6,31 @@
 #    By: qduperon <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/06/17 12:25:58 by qduperon          #+#    #+#              #
-#    Updated: 2016/06/17 17:00:45 by qduperon         ###   ########.fr        #
+#    Updated: 2016/06/17 17:43:51 by qduperon         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!bin/zsh
 
-echo "write the adresse of the git :"
+cd ..
+echo "\033[32mwrite the adresse of the git :\033[00m"
 read -r adresse
-echo "write the name of the folder :"
+echo "\033[32mwrite the name of the folder :\033[00m"
 read -r name
 git clone $adresse $name
-
 cd $name
-echo "creation folders..."
+echo "\033[32mcreation folders...\033[00m"
 mkdir srcs
 mkdir includes
-echo "done"
+echo "\033[31mdone\033[00m"
+echo "\033[32mcreation header\033[00m"
 touch $name.h
 mv $name.h includes
-touch Makefile
-cd ..
-cp -R libft $name
+echo "\033[31mdone\033[00m"
+cd ../script/
+echo "\033[32mcreation library\033[00m"
+cp -R libft ../$name
+echo "\033[31mdone\033[00m"
+echo "\033[32mcreation Makefile\033[00m"
+cp Makefile ../$name
+echo "\033[31mdone\033[00m"
